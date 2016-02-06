3dfc5ba358569efa37a09e3ca0ede20652fbfe30
James Thomas <james@just-james.com>
2016-02-06 15:55:29 -0600

    auth messed up
    fixing AUTH_EVENTS

--- client/app/scripts/configuration.js
+++ client/app/scripts/configuration.js
@@ -2,13 +2,6 @@
 
  angular.module('config', [])
 
-.constant('ENV', {
-  name:'development',apiEndpoint:'http://Jamess-MacBook-Air.local:8080/'
-})
-
-.constant('AUTH_EVENTS', {
-    notAuthenticated: 'auth-not-authenticated',
-    notAuthorized: 'auth-not-authorized'
-})
+.constant('ENV', {name:'development',apiEndpoint:'http://dev.yoursite.com:10000/'})
 
 ;
