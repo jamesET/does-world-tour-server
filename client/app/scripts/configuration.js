edb8d7b5b1a52769663002036af7b203c8481d39
James Thomas <james@just-james.com>
2016-02-06 15:57:39 -0600

    AUTH_EVENTS
    Commit AUTH_EVENTS

--- client/app/scripts/configuration.js
+++ client/app/scripts/configuration.js
@@ -4,4 +4,10 @@
 
 .constant('ENV', {name:'development',apiEndpoint:'http://dev.yoursite.com:10000/'})
 
+.constant('AUTH_EVENTS', {
+    notAuthenticated: 'auth-not-authenticated',
+    notAuthorized: 'auth-not-authorized'
+})
+
+
 ;
