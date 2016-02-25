7ddfb377c8f0b7f50efcec20e7905ac2b542725c
James Thomas <james@just-james.com>
2016-02-25 10:47:53 -0600

    Start screen gradient change

--- client/www/js/configuration.js
+++ client/www/js/configuration.js
@@ -2,7 +2,7 @@
 
  angular.module('config', [])
 
-//.constant('ENV', {name:'development',apiEndpoint:'http://localhost:8080/'})
-.constant('ENV', {name:'production',apiEndpoint:'http://beerlist-env.us-west-2.elasticbeanstalk.com/'})
+.constant('ENV', {name:'development',apiEndpoint:'http://localhost:8080/'})
+//.constant('ENV', {name:'production',apiEndpoint:'http://beerlist-env.us-west-2.elasticbeanstalk.com/'})
 
 ;
