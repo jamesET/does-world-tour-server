7894a8f56db98932fde963895c870d1fb733d4fe
James Thomas <james@just-james.com>
2016-02-25 16:43:32 -0600

    updated code style in app.auth

--- client/www/js/auth.js
+++ client/www/js/auth.js
@@ -1,90 +1,93 @@
-'use strict';
+(function() {
+    'use strict';
 
-(function (angular) {
+    angular
+        .module('app.auth',['app.core'])
+        .service('auth', AuthService);
 
-  function AuthService($http, $q, session, ENV, logger, exception){
-    var baseUrl = ENV.apiEndpoint;
-    var loginUrl = baseUrl + '/login/';
-    var logoutUrl =  baseUrl + '/logout/';
-    var isAuthenticated = false;
+    AuthService.$inject = ['$http', '$q', 'session','ENV','logger','exception'];
 
-    /**
-    * Check whether the user is logged in
-    * @returns boolean
-    */
-    this.isLoggedIn = function isLoggedIn(){
-      return session.getUser() !== null;
-    };
+    /* @ngInject */
+    function AuthService($http, $q, session, ENV, logger, exception) {
+        this.logIn = logIn;
+        this.logOut = logOut;
+        this.isLoggedIn = isLoggedIn;
+        this.isAuthenticated = isAuthenticated;
+        this.isAdmin = isAdmin;
 
-    /**
-    * Log in
-    *
-    * @param credentials
-    * @returns {*|Promise}
-    */
-    this.logIn = function(username,password){
-      session.setEmail(username);
-      session.setPassword(password);
-      var credentials = {
-        username : username,
-        password : password
-      };
-      return $http
-        .post(loginUrl, credentials)
-        .then( loginSuccess )
-        .catch( exception.catcher('Login failed') );
+        var baseUrl = ENV.apiEndpoint;
+        var loginUrl = baseUrl + '/login/';
+        var logoutUrl =  baseUrl + '/logout/';
+        var isAuthenticated = false;
 
-        function loginSuccess(response, status, headers, config) {
-            isAuthenticated = true;
-            session.setUser(response.data.userId);
-            session.setAccessToken(response.data.token);
-            session.setUserData(response.data.userTO);
-            session.setRole(response.data.userTO.role);
+        /**
+        * Check whether the user is logged in
+        * @returns boolean
+        */
+        function isLoggedIn() {
+            return session.getUser() !== null;
         }
 
-    };
+        /**
+        * Log in
+        *
+        * @param credentials
+        * @returns {*|Promise}
+        */
+        function logIn(username,password) {
+          session.setEmail(username);
+          session.setPassword(password);
+          var credentials = {
+            username : username,
+            password : password
+          };
+          return $http
+            .post(loginUrl, credentials)
+            .then( loginSuccess )
+            .catch( exception.catcher('Login failed') );
 
-    /**
-    * Log out
-    *
-    * @returns {*|Promise}
-    */
-    this.logOut = function(){
-      isAuthenticated = false;
-      session.destroy();
-      return $http
-        .post(logoutUrl)
-        .then(logOutComplete)
-        .catch(exception.catcher('Logout failed'));
-
-        function logOutComplete() {
-            logger.log('User logged out');
+            function loginSuccess(response, status, headers, config) {
+                isAuthenticated = true;
+                session.setUser(response.data.userId);
+                session.setAccessToken(response.data.token);
+                session.setUserData(response.data.userTO);
+                session.setRole(response.data.userTO.role);
+            }
         }
-    };
 
-    this.isAuthenticated = function() {
-      return isAuthenticated;
-    };
+        /**
+        * Log out
+        *
+        * @returns {*|Promise}
+        */
+        function logOut() {
+          isAuthenticated = false;
+          session.destroy();
+          return $http
+            .post(logoutUrl)
+            .then(logOutComplete)
+            .catch(exception.catcher('Logout failed'));
 
-    this.isAdmin = function() {
-      if (!isAuthenticated) {
-        return false;
-      }
-        if (session.getRole() === 'ADMIN') {
-          return true;
-        } else {
-          return false;
+            function logOutComplete() {
+                logger.log('User logged out');
+            }
         }
-    };
 
-  }
+        function isAuthenticated() {
+          return isAuthenticated;
+        }
 
-  // Inject dependencies
-  AuthService.$inject = ['$http', '$q', 'session','ENV','logger','exception'];
+        function isAdmin() {
+          if (!isAuthenticated) {
+            return false;
+          }
+            if (session.getRole() === 'ADMIN') {
+              return true;
+            } else {
+              return false;
+            }
+        }
 
-  // Export
-  angular
-    .module('app.auth',['app.core'])
-    .service('auth', AuthService);
 
-})(angular);
+    }
+})();
