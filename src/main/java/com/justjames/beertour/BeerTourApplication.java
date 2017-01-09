59b896ffbd9779d95833ca3ef12be8dd268948e2
James Thomas <jimmy.e.thomas@gmail.com>
2017-01-09 13:58:46 -0700

    Replaced deprecated ServletContextInitializer

--- src/main/java/com/justjames/beertour/BeerTourApplication.java
+++ src/main/java/com/justjames/beertour/BeerTourApplication.java
@@ -20,7 +20,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jersey.JerseyAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
 import com.justjames.beertour.security.PublicFilter;
--- src/main/java/com/justjames/beertour/JavaMelodyConfiguration.java
+++ src/main/java/com/justjames/beertour/JavaMelodyConfiguration.java
@@ -5,7 +5,7 @@ import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 
 import org.springframework.boot.context.embedded.FilterRegistrationBean;
-import org.springframework.boot.context.embedded.ServletContextInitializer;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
